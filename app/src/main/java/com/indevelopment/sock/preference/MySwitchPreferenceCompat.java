055e03bffb5ea703bc84e46a7b17dfe28cbfe255
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2019-06-06 16:23:04 +0700

    Adapt SwitchPreference summary on both theme.

--- app/src/main/java/com/indevelopment/sock/preference/MySwitchPreferenceCompat.java
+++ app/src/main/java/com/indevelopment/sock/preference/MySwitchPreferenceCompat.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.PreferenceViewHolder;
 import androidx.preference.SwitchPreferenceCompat;
 
+import com.indevelopment.sock.adapter.RuleAdapter;
+
 public class MySwitchPreferenceCompat extends SwitchPreferenceCompat {
     public MySwitchPreferenceCompat(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
@@ -29,10 +31,15 @@ public class MySwitchPreferenceCompat extends SwitchPreferenceCompat {
     public void onBindViewHolder(PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
         TextView title = (TextView) holder.findViewById(android.R.id.title);
+        TextView summary = (TextView) holder.findViewById(android.R.id.summary);
+
+        summary.setAlpha(RuleAdapter.MEDIUM_EMPHASIZE_TEXT);
         if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             title.setTextColor(getContext().getResources().getColor(android.R.color.white));
+            summary.setTextColor(getContext().getResources().getColor(android.R.color.white));
         } else {
             title.setTextColor(getContext().getResources().getColor(android.R.color.black));
+            summary.setTextColor(getContext().getResources().getColor(android.R.color.black));
         }
     }
 }
