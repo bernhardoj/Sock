93023184fd648bd5fd08b21235edc271fe13c7af
Bernhard Owen Josephus <bernhard.josephus@yahoo.co.id>
2019-06-06 22:24:47 +0700

    Add License list preference listener to show all library with its license

--- app/src/main/java/com/indevelopment/sock/activity/SettingsActivity.java
+++ app/src/main/java/com/indevelopment/sock/activity/SettingsActivity.java
@@ -1,19 +1,30 @@
 package com.indevelopment.sock.activity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.SwitchPreferenceCompat;
 
+import com.indevelopment.sock.BillingManager;
 import com.indevelopment.sock.R;
+import com.indevelopment.sock.adapter.MyArrayAdapter;
+import com.indevelopment.sock.data.LicenseData;
 
 public class SettingsActivity extends AppCompatActivity {
+    private static final String DARK_MODE_KEY = "dark_mode";
+    private static final String UNLOCK_PREMIUM_KEY = "unlock_premium";
+    private static final String LICENSES_KEY = "licenses";
+    private static final String RATE_APP_KEY = "rate_app";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +47,16 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragmentCompat {
+    public static class SettingsFragment extends PreferenceFragmentCompat{
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
-            final SwitchPreferenceCompat darkModeSwitch = findPreference("dark_mode");
+            final SwitchPreferenceCompat darkModeSwitch = findPreference(DARK_MODE_KEY);
+            final Preference unlockPremium = findPreference(UNLOCK_PREMIUM_KEY);
+            final Preference licenseView = findPreference(LICENSES_KEY);
+            final Preference rateApp = findPreference(RATE_APP_KEY);
 
+            // Handle the dark mode here
             if(darkModeSwitch != null) {
                 if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
                     darkModeSwitch.setChecked(true);
@@ -61,6 +76,67 @@ public class SettingsActivity extends AppCompatActivity {
                             restartActivity();
                         }
                         return false;
+                    }
+                });
+            }
+
+            // Handle the Google Play billing here
+            if(unlockPremium != null) {
+                unlockPremium.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        new BillingManager(getActivity());
+                        return true;
+                    }
+                });
+            }
+
+            if(licenseView != null) {
+                licenseView.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        Activity activity = getActivity();
+                        if(activity != null) {
+                            final MyArrayAdapter adapter =
+                                    new MyArrayAdapter(activity, R.layout.license_layout, LicenseData.generateLicense());
+
+                            new AlertDialog.Builder(activity)
+                                    .setTitle("License")
+                                    .setAdapter(adapter, new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            // Intent to browser here
+                                            String targetUrl = null;
+                                            switch (which) {
+                                                case 0:
+                                                    targetUrl = "https://github.com/google/gson";
+                                                    break;
+                                                case 1:
+                                                    targetUrl = "https://github.com/maoni-app/maoni";
+                                                    break;
+                                            }
+                                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                                            intent.setData(Uri.parse(targetUrl));
+                                            startActivity(intent);
+                                        }
+                                    })
+                                    .create()
+                                    .show();
+                            return true;
+                        }
+
+                        return false;
+                    }
+                });
+            }
+
+            if(rateApp != null) {
+                rateApp.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                    @Override
+                    public boolean onPreferenceClick(Preference preference) {
+                        // Intent to App Store here
+
+                        return true;
                     }
                 });
             }
